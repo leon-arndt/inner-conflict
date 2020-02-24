7482d860efdc5f0044ce2d510946b317b3b83918
Leon Arndt <neotalon27@gmail.com>
2020-02-24 18:54:26 +0100

    Add camera following

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Follow the player
+public class FollowCam : MonoBehaviour
+{
+    public bool followPlayer = false;
+    public float posSmoothTime = 0.2f;
+    private Vector3 moveVelocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!followPlayer) return;
+
+        //position on player
+        Vector3 targetPos = CharacterController2D.Instance.transform.position;
+        Vector3 smoothPos = Vector3.SmoothDamp(transform.position, targetPos, ref moveVelocity, posSmoothTime);
+        float zPos = transform.position.z;
+        transform.position = new Vector3(smoothPos.x, smoothPos.y, zPos);
+        
+    }
+}
