36fef016900186b9afc7a71f4e391b922adb950b
Leon Arndt <neotalon27@gmail.com>
2020-02-24 17:46:23 +0100

    Fix landing on any side

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -55,7 +55,8 @@ public class CharacterController2D : MonoBehaviour
 
         // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
         // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        Vector2 groundPosition = transform.position - Vector3.up;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundPosition, k_GroundedRadius, m_WhatIsGround);
         for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].gameObject != gameObject)
